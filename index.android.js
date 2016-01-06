90933286ad199e5b0515b98d254b36c7dd5ed2a9
Tim Buckley <timothypbuckley@gmail.com>
2016-01-06 12:21:51 -0500

    Swap out stubbed index.android.js with index.ios.js. Test Later

--- index.android.js
+++ index.android.js
@@ -1,52 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+'use strict'
 
-var React = require('react-native');
-var {
+const Api = require('./src/api')
+const React = require('react-native')
+const {
   AppRegistry,
-  StyleSheet,
-  Text,
   View,
-} = React;
+  Text,
+  MapView,
+  StyleSheet
+} = React
 
-var weatherblah = React.createClass({
+const APIKEY = "058e0caee9fe24d810aa10039a9fc8fb"
+
+const Weather = React.createClass({
+  getInitialState: function() {
+      return {
+        pin: {
+          latitude: 37,
+          longitude: -95
+        },
+        city: '',
+        temperature: '',
+        description: ''
+      }
+  },
   render: function() {
-    return (
+    return(
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <MapView
+          annotations={[this.state.pin]}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+          style={styles.map}>
+        </MapView>
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>{this.state.city}</Text>
+          <Text style={styles.text}>{this.state.temperature}</Text>
+          <Text style={styles.text}>{this.state.description}</Text>
+        </View>
       </View>
-    );
-  }
-});
+    )
+  },
+  onRegionChangeComplete: function(region) {
+    this.setState({
+      pin: {
+        latitude: region.latitude,
+        longitude: region.longitude
+      }
+    });
 
-var styles = StyleSheet.create({
+    Api(region.latitude, region.longitude)
+      .then((data) => {
+        console.log(data)
+        this.setState(data);
+      });
+  }
+})
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'stretch',
+    // backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  map: {
+    flex: 20,
+    marginTop: 30
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  textWrapper: {
+    flex: 10,
+    alignItems: 'center'
   },
-});
+  text: {
+    fontSize: 30
+  }
+})
 
-AppRegistry.registerComponent('weatherblah', () => weatherblah);
+AppRegistry.registerComponent('weatherblah', () => Weather)
