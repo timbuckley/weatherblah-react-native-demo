971685793efc0a65dc5851725b707ce7d49348fa
Tim Buckley <timothypbuckley@gmail.com>
2016-01-06 12:23:14 -0500

    Add pin to map, call to api, more styling

--- index.ios.js
+++ index.ios.js
@@ -1,24 +1,76 @@
 'use strict'
 
+const Api = require('./src/api')
 const React = require('react-native')
 const {
   AppRegistry,
   View,
+  Text,
   MapView,
   StyleSheet
 } = React
 
 const Weather = React.createClass({
+  getInitialState: function() {
+      return {
+        pin: {
+          latitude: 37,
+          longitude: -95
+        },
+        city: '',
+        temperature: '',
+        description: ''
+      }
+  },
   render: function() {
     return(
-      <MapView style={styles.map}>
-      </MapView>)
+      <View style={styles.container}>
+        <MapView
+          annotations={[this.state.pin]}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+          style={styles.map}>
+        </MapView>
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>{this.state.city}</Text>
+          <Text style={styles.text}>{this.state.temperature}</Text>
+          <Text style={styles.text}>{this.state.description}</Text>
+        </View>
+      </View>
+    )
+  },
+  onRegionChangeComplete: function(region) {
+    this.setState({
+      pin: {
+        latitude: region.latitude,
+        longitude: region.longitude
+      }
+    });
+
+    Api(region.latitude, region.longitude)
+      .then((data) => {
+        console.log(data)
+        this.setState(data);
+      });
   }
 })
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    // backgroundColor: '#F5FCFF'
+  },
   map: {
-    flex: 1
+    flex: 20,
+    marginTop: 30
+  },
+  textWrapper: {
+    flex: 10,
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 30
   }
 })
 
